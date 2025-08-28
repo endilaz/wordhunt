ec2408fc4d472bad7e8400a965f04c187fadb934
endilaz <danielzhangwa@gmail.com>
2025-08-27 21:44:36 -0700

    cleaned up a lot. implemented functional anagrams solver too

---
+++ anagrams.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define MIN_LENGTH 3
+#define DEBUG false
+
+using namespace std;
+
+vector<int> scores = {0, 0, 0, 100, 400, 800, 1400, 1800, 2200, 2600, 3000, 3400, 3800, 4200, 4600, 5000};
+string wordlist = "CSW21.txt";
+ifstream wordlist_file;
+string history = "anagrams_log.txt";
+ofstream history_file(history, ios_base::app);
+set<string> words;
+
+struct comp {
+    bool operator()(string a, string b) const {
+        if (a.size() == b.size()) return a < b;
+        return a.size() > b.size();
+    }
+};
+
+string getCurrentDateTimeString() {
+    stringstream ss;
+    
+    std::time_t now = std::time(nullptr);
+    std::tm* localTime = std::localtime(&now);
+
+    ss << std::setfill('0') << std::setw(2) << 1 + localTime->tm_mon << "/" << std::setw(2) << localTime->tm_mday << "/"
+       << 1900 + localTime->tm_year << " " << std::setw(2) << localTime->tm_hour << ":" << std::setw(2) << localTime->tm_min;
+
+    return ss.str();
+}
+
+template <typename T>
+int find_anagrams(string substr, string board, int size, ll mask, T& valid_words) {
+    int score = 0;
+    if (DEBUG) cout << substr << " " << board << " " << size << " " << mask << endl;
+    if (words.find(substr) != words.end()) {
+        if (valid_words.insert(substr).second) 
+            if (substr.size() < scores.size()) 
+                score += scores[substr.size()];
+    }
+    for (int i = 0; i < size; i++) {
+        if ((((1 << i) & mask) == 0)) {
+            substr.push_back(board[i]);
+            string next = (*(words.lower_bound(substr)));
+            if (DEBUG) cout << "NEXT: " << next << endl;
+            if (next.size() >= substr.size() && next[substr.size()-1] == board[i]) {
+                score += find_anagrams(substr, board, size, mask | (1 << i), valid_words);
+            }
+            substr.pop_back();
+        }
+    }
+    return score;
+}
+
+set<string, comp> valid_words;
+int find_anagrams(string board) {
+    valid_words.clear();
+    return find_anagrams("", board, board.size(), 0, valid_words);
+}
+
+void solve(bool inputting) {
+    while (inputting) {
+        int points = 0, min_length = 3;
+        string board, notes;
+        cout << "please input board, notes, min length to print: " << flush;
+        cin >> board >> notes >> min_length;
+        std::transform(board.begin(), board.end(), board.begin(), ::toupper);
+        if (board == "QUIT") return;
+
+        points = find_anagrams(board);
+        cout << endl << points << endl;
+        history_file << board << " " << points << " | " << notes << " " << getCurrentDateTimeString() << endl;
+        
+        string prev = *(valid_words.begin());
+        for (string s : valid_words) {
+            if (s.size() < scores.size()) points -= scores[s.size()];
+            if (s.size() < min_length) continue;
+            if (s.size() != prev.size()) cout << endl;
+            if (s.size() != prev.size()) history_file << endl;
+            cout << s << " ";
+            history_file << s << " ";
+            prev = s;
+        }
+        assert(points == 0);
+        cout << endl;
+        history_file << endl << endl;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    wordlist_file.open(wordlist);
+    string word;
+    while (wordlist_file >> word) {
+        words.insert(word);
+    }
+    wordlist_file.close();
+
+    solve(1);
+    history_file << "=======================================" << endl;
+    history_file.close();
+    return 0;
+}
--- wordhuntsolver.cpp
+++ wordhuntsolver.cpp
@@ -2,16 +2,10 @@
 #define ll long long
 
 using namespace std;
-// mitm?
-// just do simple dfs from each tile, no need to do fancy dp?
-// prune early if binary search return value [i] != word[i] (aka no word can be formed after this combo) (reverse the word order tho, so ending at tile instead of starting at tile)
-//int n = 200000;
+
 const int board_rows = 4, board_cols = 4;
 string board[board_rows][board_cols];
-// string placeholder(board_cols, 'A');
-// vector<string> board(board_rows, placeholder);
-set<string> words;// valid_words;
-// vector<vector<set<pair<string, ll>>>> combos;
+set<string> words;
 
 ll coord_to_mask(int row, int col) {
     return (1 << (row * board_cols + col));
@@ -30,9 +24,6 @@ vector<pair<int, int>> find_neighbors(int row, int col) {
     return neighbors;
 }
 
-// bool comp(string a, string b) {
-//     return a.size() < b.size();
-// }
 struct comp {
     bool operator()(string a, string b) const {
         if (a.size() == b.size()) return a < b;
@@ -54,7 +45,6 @@ vector<vector<vector<set<pair<string, ll>>>>> generate_combos_dp(int target_leng
     
     for (int row = 0; row < board_rows; ++row) {
         for (int col = 0; col < board_cols; ++col) {
-            //cout << row << col << endl;
             generated[0][row][col].emplace(board[row][col], coord_to_mask(row, col));
         }
     }
@@ -68,12 +58,10 @@ vector<vector<vector<set<pair<string, ll>>>>> generate_combos_dp(int target_leng
                     tie(neighbor_row, neighbor_col) = n;
                     auto neighbor_mask = coord_to_mask(neighbor_row, neighbor_col);
                     for (auto& short_item : generated[i-1][row][col]) {
-                        //if (short_item.first.size() < target_length-1) continue;
                         if (neighbor_mask & short_item.second) {
                             continue;
                         }
                         string closest_next_word = *(words.lower_bound(short_item.first));
-                        //cout << short_item.first << " " << closest_next_word << "" << i << "\n";
                         if ((closest_next_word.size() < i) | (closest_next_word[i-1] != (*short_item.first.rbegin()))) continue;
                         generated[i][neighbor_row][neighbor_col].emplace((short_item.first + board[neighbor_row][neighbor_col]), (short_item.second | neighbor_mask));
                     }
@@ -84,89 +72,7 @@ vector<vector<vector<set<pair<string, ll>>>>> generate_combos_dp(int target_leng
     return generated;
 }
 
-/*
-// [row][col] -> [words pairs], [row][col][index] -> specific word pair
-// vector<vector<set<pair<string, ll>>>> generate_combos(int target_length) {
-//     vector<vector<set<pair<string, ll>>>> curr_list(board_rows);
-//     for (int row = 0; row < board_rows; ++row) {
-//         curr_list[row].resize(board_cols);
-//         for (int col = 0; col < board_cols; ++col) {
-//             curr_list[row][col].emplace(board[row][col], coord_to_mask(row, col));
-//         }
-//     }
-
-//     if (target_length == 1) {
-//         return curr_list;
-//     }
-
-//     auto shorter = generate_combos(target_length - 1); // O(n!)
-//     for (int row = 0; row < board_rows; ++row) {
-//         for (int col = 0; col < board_cols; ++col) {
-//             auto neighbors = find_neighbors(row, col);
-//             for (auto& n : neighbors) {
-//                 int neighbor_row, neighbor_col;
-//                 tie(neighbor_row, neighbor_col) = n;
-//                 for (auto& short_item : shorter[row][col]) {
-//                     if (short_item.first.size() < target_length-1) continue;
-//                     auto neighbor_mask = coord_to_mask(neighbor_row, neighbor_col);
-//                     if (neighbor_mask & short_item.second) {
-//                         continue;
-//                     }
-//                     curr_list[neighbor_row][neighbor_col].emplace(board[neighbor_row][neighbor_col] + short_item.first, short_item.second | neighbor_mask);
-//                 }
-//             }
-//         }
-//     }
-
-//     // for (int row = 0; row < board_rows; ++row) {
-//     //     for (int col = 0; col < board_cols; ++col) {
-//     //         for (auto& w : curr_list[row][col])
-//     //             shorter[row][col].insert(w);
-//     //     }
-//     // }
-
-//     return shorter;
-// }
-
-// vector<vector<set<pair<string, ll>>>> merge(int targ) {
-//     vector<vector<set<pair<string, ll>>>> curr_list(board_rows);
-//     for (int row = 0; row < board_rows; ++row) {
-//         curr_list[row].resize(board_cols);
-//         for (int col = 0; col < board_cols; ++col) {
-//             curr_list[row][col].emplace(board[row][col], coord_to_mask(row, col));
-//         }
-//     }
-    
-//     auto half = generate_combos(targ / 2);
-
-//     for (int row = 0; row < board_rows; ++row) {
-//         for (int col = 0; col < board_cols; ++col) {
-//             auto neighbors = find_neighbors(row, col);
-//             for (auto& n : neighbors) {
-//                 int neighbor_row, neighbor_col;
-//                 tie(neighbor_row, neighbor_col) = n;
-//                 for (auto& h1 : half[row][col]) {
-//                     string first_half_copy = h1.first;
-//                     reverse(first_half_copy.begin(), first_half_copy.end());
-//                     // reverse(h1.first.begin(), h1.first.end());
-//                     for (auto& h2 : half[neighbor_row][neighbor_col]) {
-//                         if (h1.second & h2.second) continue;
-//                         // string new_word = first_half_copy + h2.first;
-//                         // ll new_mask = h1.second | h2.second;
-//                         // pair<string, ll> comb = make_pair(new_word, new_mask);
-//                         curr_list[row][col].emplace(first_half_copy + h2.first, h1.second | h2.second);
-//                     }
-//                     // reverse(h1.first.begin(), h1.first.end());
-//                 }
-//             }
-//         }
-//     }
-
-//     return curr_list;
-// }
-*/
-
-set<string, comp> solve_board(int k) { // O(logKlogW) * 
+set<string, comp> solve_board(int k) { 
     auto generated = generate_combos_dp(k);
     set<string, comp> valid_words;
     for (int i = 0; i < k; i++) { // i = leng-1
@@ -174,8 +80,7 @@ set<string, comp> solve_board(int k) { // O(logKlogW) *
             for (int col = 0; col < board_cols; ++col) {
                 for (auto& combo : generated[i][row][col]) {
                     string word = combo.first;
-                    //cout << word << " " << i << endl;
-                    if (words.find(word) != words.end()) {//binary_search(words.begin(), words.end(), word)) { // O(logK)
+                    if (words.find(word) != words.end()) {
                         valid_words.insert(combo.first); // O(logW)
                     }
                 }
@@ -189,7 +94,6 @@ vector<double> cum_letter_weights(26);
 double weight_sum = 0;
 void generate_board() {
     std::uniform_real_distribution<double> unif(0,weight_sum);
-    //default_random_engine re;
     std::mt19937 re(std::time(0));
     for (int i = 0; i < board_cols * board_rows; i++) {
         double random_double = unif(re);
@@ -303,15 +207,6 @@ int main() {
     }
     weight_sum = cum_letter_weights[cum_letter_weights.size()-1];
 
-    // generate_board();
-    // string_to_board("TOINATAELERHHDSS"); // 285700
-    // string_to_board("MHONITERLASNSERU"); // wr 542000; HORSETAILS
-    // string_to_board("PNROIOHAUGEEGALU"); // 43900
-    // string_to_board("IAPNWREUAOHSETSH"); // 195500
-    // string_to_board("ROWAPBOHKAYBTTIG"); // 34800
-    // string_to_board("CBBWIRGITOUETSNS"); // 188500
-    // string_to_board("DRONCTSERETFODLI"); // 263200
-    // string_to_board("DSLSMDETWOTEGLPI"); // 139600
     bool inputting = true, lst = false;
     if (inputting) cin >> board_string;
     while (inputting) {
